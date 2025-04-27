83124dfd90824bb022800b1796e239a39fde3f08
nandakumarst <nandakumarst68@gmail.com>
2025-04-27 10:29:53 +0530

    Update configuration for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'vercel.app'],
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+};
 
 export default nextConfig;
